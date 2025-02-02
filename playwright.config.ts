defa9a1000ee38b36a1204b8aea63e1cabb69219
Maayan <maayan14489@gmail.com>
2025-02-02 22:15:47 +0200

    h

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,14 +12,14 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  globalTimeout: 60 * 1000,
+  // globalTimeout: 60 * 1000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -30,8 +30,16 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    
     trace: 'on-first-retry',
+    ...devices['Desktop Chrome'],
+    viewport: null,
+    launchOptions: {
+      args: ['--start-maximized']
+    },
   },
+  
+  
 
   /* Configure projects for major browsers */
   projects: [
